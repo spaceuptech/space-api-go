6acc48af5104d846ba871663ea9a63015ac1a15e
Aliabbas Merchant <merchantaliabbas@gmail.com>
2019-07-24 10:47:26 +0530

    Update Port in Examples

--- profiling/service.go
+++ profiling/service.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	api, err := api.New("books-app", "localhost:8081", false)
+	api, err := api.New("books-app", "localhost:4124", false)
 	if(err != nil) {
 		fmt.Println(err)
 	}
