2f14d47e39572400d081fa1aa00f9ffd0a5283b1
Master <sharadregoti15@gmail.com>
2020-01-07 11:18:01 +0530

    realtime (#23)

--- api/realtime/subscription.go
+++ api/realtime/subscription.go
@@ -6,11 +6,15 @@ import (
 
 // LiveQuerySubscription represents the realtime subscription
 type LiveQuerySubscription struct {
-	unsubscribeFunc UnsubscribeFunction
-	snapshot        *model.LiveData
+	unsubscribeFunc func()
+	snapshot        model.SnapshotFunction
 }
 
-func (l *LiveQuerySubscription) GetSnapshot() *model.LiveData {
+func LiveQuerySubscriptionInit(snapshot model.SnapshotFunction, unsubscribeFunc func()) *LiveQuerySubscription {
+	return &LiveQuerySubscription{unsubscribeFunc: unsubscribeFunc, snapshot: snapshot}
+}
+
+func (l *LiveQuerySubscription) GetSnapshot() model.SnapshotFunction {
 	return l.snapshot
 }
 
